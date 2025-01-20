34bdd675160e094bdf5e075dd8d57cdbb202055b
Douglas Silva <douglas.ernesto.silva@gmail.com>
2025-01-20 00:10:51 -0300

    Resolvendo a issue e implementando documentação.

--- ex03.py
+++ ex03.py
@@ -29,9 +29,26 @@ animais = [
     },
     
 ]
-
+##Criação da rota para buscar todos os animais da lista
 @app.get('/animais')
 def listar_todos_animais():
+    """Função para listar todos os animais
+    """
+    #Se o tamanho da lista for igual a  ele retorna um erro, se não ele retonra uma resposta em JSON.
     if len(animais) == 0:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     return JSONResponse(content=animais, status_code=status.HTTP_200_OK)
+##Criação da rota para busar um animal espefífico da lista.
+@app.get('/animais/{id}')
+def listar_animal_especifico(id:int):
+    """Função para percorrer a lista de animais
+    """
+    #Para cada animal na lista, verifica se o parametro é igual a query desejada.
+    for animal in animais:
+        ### animal.get('id') - Query
+        ### id - Param
+        if animal.get('id') == id:
+            return animal
+    return JSONResponse(
+        content = {'message':'Animal não encontrado'},
+        status_code = status.HTTP_404_NOT_FOUND)

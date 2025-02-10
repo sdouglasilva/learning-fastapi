3b7fee790a89329696bbaa6833e82542c6de6e6a
Leonardonascimentodossantos <leonardo_n.s@hotmail.com>
2025-02-10 14:58:52 -0300

    feat: terminando api-cadastro-de-pessoas

--- aula02/exercises/api-cadastro-de-pessoas/main.py
+++ aula02/exercises/api-cadastro-de-pessoas/main.py
@@ -1,29 +1,30 @@
 from fastapi import FastAPI, status
-from models import Person
 from schemas import PersonIn,MessageOut
-from pydantic import BaseModel
-from pydantic import ValidationError
+
 from fastapi.responses import JSONResponse
 
 
-persons = []
+persons = [{
+  "name": "string",
+  "age": 0,
+  "heigth": 0,
+  "gender": "M"
+}]
 app  = FastAPI()
 
 @app.get('/pessoas/listar',response_model=MessageOut)
-def list_all_persons():
-  try:
-      return JSONResponse(content=persons, status_code=status.HTTP_200_OK)
-  except ValidationError as error:
-      if len(persons) == 0:
-        return {"message":f'Nenhuma pessoa cadastrada na lista{error}'}
+def list_all_persons() ->MessageOut:
+  if not persons:
+     return JSONResponse(content={"message":"Nenhuma pessoa cadastrada"},status_code=status.HTTP_204_NO_CONTENT)
+  print(persons)
+  return JSONResponse(content={"pessoas":[person for person in persons]},status_code=status.HTTP_200_OK)
+     
       
 @app.post('/pessoas/cadastrar', response_model=MessageOut)
 def create_new_person(person:PersonIn)-> MessageOut:
-    try:  
-      new_person = Person(**person.model_dump())
-      persons.append(new_person)
+    new_person = person.model_dump()
+    persons.append(new_person)
 
-      return {'message':'Pessoa cadastrada com sucesso'}
-    except ValidationError as error:
-      return {"message":f"Erro de validação{error}"}
+    return MessageOut(message="Pessoa cadastrada com sucesso.")
+
     

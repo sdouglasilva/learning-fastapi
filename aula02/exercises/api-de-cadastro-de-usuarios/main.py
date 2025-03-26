6f0a7d7e8d4541206a1d276e43b7448fcfb50b90
sdouglasilva <douglas.ernesto.silva@gmail.com>
2025-03-26 16:47:13 -0300

    fix: corrigindo falhas no método get

--- aula02/exercises/api-de-cadastro-de-usuarios/main.py
+++ aula02/exercises/api-de-cadastro-de-usuarios/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from models import User
 from schemas import UserIn, UserOut, MessageOut
+from uuid import UUID
 
 app = FastAPI()
 
@@ -16,18 +17,26 @@ def cadastrar_usuario(user:UserIn)->MessageOut:
     users_list.append(new_user)
     return MessageOut(message='Usuário cadastrado com sucesso')
 
-
 @app.get('/usuarios')
 def listar_todos_usuarios()->list[UserOut]:
     if len(users_list) == 0:
         return []
     return users_list
 
-@app.get('/users/{nome_de_usuario}')
+@app.get('/usuarios/nome:{nome_de_usuario}')
 def listar_usuario(nome_de_usuario:str)-> UserOut:
     for user_in_db in users_list:
         if user_in_db.nome == nome_de_usuario:
             return user_in_db
+
+@app.get('/usuarios/id:{uudi}', response_model=UserOut)
+def listar_usuario_uuid(uudi:UUID)->UserOut:
+    for user_in_db in users_list:
+        if user_in_db.id == uudi:
+            return user_in_db
+
+    
+
 
 
 

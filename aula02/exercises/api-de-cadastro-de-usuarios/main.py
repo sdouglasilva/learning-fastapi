15b12b136311af1db7aec4ec5a44bbbf687afeb8
sdouglasilva <douglas.ernesto.silva@gmail.com>
2025-03-27 16:35:55 -0300

    feat:finalizando crud simples 27/03/2025 - 16:35

--- aula02/exercises/api-de-cadastro-de-usuarios/main.py
+++ aula02/exercises/api-de-cadastro-de-usuarios/main.py
@@ -30,14 +30,30 @@ def listar_usuario(nome_de_usuario:str)-> UserOut:
         if user_in_db.nome == nome_de_usuario:
             return user_in_db
 
-@app.get('/usuarios/id:{uudi}', response_model=UserOut)
-def listar_usuario_uuid(uudi:UUID)->UserOut:
+@app.get('/usuarios/id:{uuid}', response_model=UserOut)
+def listar_usuario_uuid(uuid:UUID)->UserIn:
     for user_in_db in users_list:
-        if user_in_db.id == uudi:
+        if user_in_db.id == uuid:
             return user_in_db
 
-    
 
+@app.put('/alterar/usuario/{uuid}')
+def alterar_info_usuario(uuid:UUID, usuario:UserOut)-> MessageOut:
+     for user_in_db in users_list:
+        if user_in_db.id == uuid:
+            user_in_db.nome = usuario.nome
+            user_in_db.email = usuario.email
+            return MessageOut(message="Usuário alterado com sucesso")
+
+
+@app.delete('/deletar/{uuid}/usuario')
+def deletar_usuario(uuid:UUID)-> MessageOut:
+    for user_in_db in users_list:
+        if user_in_db.id == uuid:
+            users_list.remove(user_in_db)
+            return users_list, MessageOut(message="Usuário removido com sucesso")
+            
+    return MessageOut(message="Usuário não encontrado")
 
 
 
